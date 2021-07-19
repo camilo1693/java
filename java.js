e233274bba05c88569ced29f4dbe53d92e1a64e7
Camilo Aguilar <jkamilo93@hotmail.com>
2021-07-19 11:59:35 +0200

    pimer ejercicio

---
+++ java.js
@@ -0,0 +1,16 @@
+/*-------------ejercicio 1-----------------------*/
+var edad=58
+
+if (edad >59) {
+    console.log("mayor")
+}
+
+if (edad >=59) {
+    console.log ("igual")
+}
+if (edad <59) {
+    console.log("menor")
+}
+
+/*-----------------------ejercicio 2-----------------------------*/
+
