f5671a1118f8cfd30db1be9103210f4f8b56b74e
Camilo Aguilar <jkamilo93@hotmail.com>
2021-07-19 13:13:17 +0200

    2 ejercicio

--- java.js
+++ java.js
@@ -1,16 +1,26 @@
 /*-------------ejercicio 1-----------------------*/
-var edad=58
+var edad = 58;
 
-if (edad >59) {
-    console.log("mayor")
+if (edad > 59) {
+  console.log("mayor");
 }
 
-if (edad >=59) {
-    console.log ("igual")
+if (edad >= 59) {
+  console.log("igual");
 }
-if (edad <59) {
-    console.log("menor")
+if (edad < 59) {
+  console.log("menor");
 }
 
 /*-----------------------ejercicio 2-----------------------------*/
 
+function par() {
+  let numero2 = document.getElementById("numero1");
+  let numero = parseFloat(numero2.value);
+
+  if (numero % 2 == 0) {
+    document.getElementById("area").value = "el numero es par";
+  } else {
+    document.getElementById("area").value = "el numero es impar";
+  }
+}
